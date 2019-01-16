694c5ee3c09535421329a267c5d171b5c409b1cf
levy <damien.levy4@etu.univ-lorraine.fr>
2019-01-16 09:42:40 +0100

    premier triangle

--- main.cpp
+++ main.cpp
@@ -29,13 +29,12 @@ std::string split_perso(std::string mot){
 
 void read(std::string name, 
           std::vector< std::vector<float> > &coordonne, 
-          std::vector< std::vector<int> > &point){
+          std::vector< std::vector<int> > &pnt){
   //std::vector< std::vector<float> > coordonne;
 
     std:: ifstream fichier(name.c_str());
     if(fichier){
-    std:: string ligne; 
-    char c;
+    std:: string ligne;
     //Tant qu'on n'est pas à la fin, on lit
     while(fichier >> ligne) { //permet de lire mot a mot
       while(ligne == "v"){
@@ -51,19 +50,18 @@ void read(std::string name,
           coordonne.push_back(point);
       }
       while(ligne == "f"){
-        
+        std::vector<int> coord; 
         for(int i = 0 ; i < 3 ; i++){
           fichier >> ligne;
           std::string s = split_perso(ligne);
-          std::cout << s <<std::endl;
+          coord.push_back(atof(s.c_str())-1);
+          //std::cout << s <<std::endl;
          
         }
-         fichier >> ligne;
+        pnt.push_back(coord);
+        fichier >> ligne;
 
-        
       }
-  
-	  
     }
   }
 
@@ -106,29 +104,80 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
   } 
 } 
 
+void triangle(std::vector<float> p1,
+              std::vector<float> p2,
+              std::vector<float> p3,
+              TGAImage &image, 
+              TGAColor color){
+                
+    line(p1[0],p1[1],p2[0],p2[1],image,color);
+    line(p2[0],p2[1],p3[0],p3[1],image,color);
+    line(p3[0],p3[1],p1[0],p1[1],image,color);
+
+}
 
 
-
-int main(int argc, char** argv) {
-  TGAImage image(800, 800, TGAImage::RGB);
+int main() {//int argc, char** argv
+  int const width = 800;
+  int const height = 800;
+  TGAImage image(width, height, TGAImage::RGB);
   
   std::vector< std::vector<float> > coordonne;
   std::vector< std::vector<int> > pnt;
   read("african_head.obj",coordonne,pnt);
-  std::vector<float> point;
-  int size = coordonne.size(); 
-  for(int i = 0 ; i < size; i++){
+  std::vector<float> point; // pour recuperer les coordonne
+  std::vector<int> p; //pour recuperer les 3 point à relier
+  int size_coordonne = coordonne.size(); 
+  int size_pnt = pnt.size();
+  
+  //dessine l'image en fils de fer
+  /*
+  for(int i = 0 ; i < size_pnt ; i++ ){
+   
+    p = pnt[i];
+
+    for(int j=0;j<3;j++){
+      point = coordonne[p[j]];
+      
+      
+      int x0 = (point[0] + 1)  * width/2;
+      int y0 = (point[1] + 1) * height/2;
+      point = coordonne[p[(j+1)%3]];
+
+      
+      int x1 = (point[0] + 1) * width/2;
+      int y1 = (point[1] + 1) * height/2;
+      line(x0 , y0 , x1 , y1 , image, white);
+
+    }
+  }
+*/
+  //dessine le nuage de points
+  /*for(int i = 0 ; i < size_coordonne; i++){
     point = coordonne[i];
     // std::cout <<point[0] << std::endl;
    // std::cout << ligne <<std::endl;
 
     image.set((point[0]+1)*800/2,(1+point[1])*800/2,white);
-    }
+    }*/
+
+
   //image.set(-0.000581696,-0.734665 , red);
   //  line(13, 20, 80, 40, image, white); 
   // line(20, 13, 40, 80, image, red); 
   // line(80, 40, 13, 20, image, red);
-  
+
+  std::vector<float> p1;
+  p1.push_back(13);
+  p1.push_back(20);
+  std::vector<float> p2;
+  p2.push_back(40);
+  p2.push_back(80);
+  std::vector<float> p3;
+  p3.push_back(30);
+  p3.push_back(14);
+  triangle(p1,p2,p3,image,white);
+
   image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
   image.write_tga_file("output.tga");
   return 0;
